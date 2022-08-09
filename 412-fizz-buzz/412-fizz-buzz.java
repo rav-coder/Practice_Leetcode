711fa45f759707c9f2756ce637ef604afa21a4f9
Saurav <76596911+rav-coder@users.noreply.github.com>
2022-08-08 20:09:29 -0600

    Time: 3 ms (33.80%), Space: 48.2 MB (59.30%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.java
+++ 412-fizz-buzz/412-fizz-buzz.java
@@ -3,12 +3,15 @@ class Solution {
         List<String> ans = new ArrayList <String> (n);
         
         for(int i = 1 ; i <= n ; i++){
+            
+            boolean div3 = (i%3 == 0);
+            boolean div5 = (i%5 == 0);
                    
-            if(i % 3 == 0 && i % 5 == 0){
+            if(div3 && div5){
                 ans.add("FizzBuzz");
-            } else if (i % 3 == 0){
+            } else if (div3){
                 ans.add("Fizz");
-            } else if ( i % 5 == 0) {
+            } else if (div5) {
                 ans.add("Buzz");
             } else{
                 ans.add(i + "");
